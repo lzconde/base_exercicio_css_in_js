a12481a6fb7b389b420fa0911e6d68a140f79372
Luiz Conde <luizconde.dev@outlook.com>
2025-04-16 22:30:09 -0300

    exercicio

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Vaga.module.css'
+import styled from 'styled-components'
 
 type Props = {
   titulo: string
@@ -10,22 +10,63 @@ type Props = {
   requisitos: string[]
 }
 
+const VagaItem = styled.li`
+  border: 1px solid var(--cor-principal);
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  padding: 16px;
+  transition: all ease 0.3s;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: var(--cor-principal);
+    color: var(--cor-secundaria);
+  }
+`
+
+const VagaTitulo = styled.h3`
+  font-weight: bold;
+  margin-bottom: 16px;
+`
+
+const VagaLink = styled.a`
+  border-color: var(--cor-secundaria);
+  background-color: var(--cor-principal);
+  color: var(--cor-secundaria);
+  display: inline-block;
+  padding: 8px 16px;
+  text-decoration: none;
+  margin-top: 16px;
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 8px;
+  text-align: center;
+
+  &:hover {
+    border-color: var(--cor-principal);
+    background-color: var(--cor-secundaria);
+    color: var(--cor-principal);
+  }
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`
+
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <VagaItem>
+    <VagaTitulo>{props.titulo}</VagaTitulo>
     <ul>
-      <li>Localizacao: {props.localizacao}</li>
+      <li>Localização: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
-      <li>Tipo de contratacao: {props.modalidade}</li>
+      <li>Tipo de contratação: {props.modalidade}</li>
       <li>
         Salário: {props.salarioMin} - {props.salarioMax}
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+    <VagaLink href="#">Ver detalhes e candidatar-se</VagaLink>
+  </VagaItem>
 )
 
 export default Vaga
